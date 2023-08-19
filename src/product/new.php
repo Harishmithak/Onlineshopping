0f5fd6a8b7a43b37131d0d3281c258cd90b6f507
harishmithak <kharishmitha@gmail.com>
2023-08-19 16:56:01 +0530

    saturday

--- src/product/new.php
+++ src/product/new.php
@@ -1,71 +1,84 @@
-<?php
-class Product {
-    private $conn;
+<?php        include("../common/nav.php");?>
 
-    public function __construct($host, $username, $password, $database) {
-        $this->conn = new mysqli($host, $username, $password, $database);
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
-    }
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product List</title>
 
-    public function getAllProducts() {
-        $sql = "SELECT * FROM Product";
-        $result = $this->conn->query($sql);
-        $products = [];
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Product List</h1>
+        <?php
 
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $products[] = $row;
-            }
-        }
+        class Product {
+            private $conn;
 
-        return $products;
-    }
-
-    public function displayProducts() {
-        $products = $this->getAllProducts();
-
-        if (count($products) > 0) {
-            echo "<table border='1'>
-                    <tr>
-                        <th>Product Code</th>
-                        <th>Product Name</th>
-                        <th>Product Type</th>
-                        <th>Description</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                        <th>Image</th>
-                    </tr>";
-
-            foreach ($products as $product) {
-                echo "<tr>
-                        <td>{$product['ProductCode']}</td>
-                        <td>{$product['ProductName']}</td>
-                        <td>{$product['ProductType']}</td>
-                        <td>{$product['Description']}</td>
-                        <td>{$product['Quantity']}</td>
-                        <td>{$product['Price']}</td>
-                        <td><img src='{$product['Image']}' alt='{$product['ProductName']}' width='100'></td>
-                    </tr>";
+            public function __construct($host, $username, $password, $database) {
+                $this->conn = new mysqli($host, $username, $password, $database);
+                if ($this->conn->connect_error) {
+                    die("Connection failed: " . $this->conn->connect_error);
+                }
             }
 
-            echo "</table>";
-        } else {
-            echo "No products found.";
+            public function getAllProducts() {
+                $sql = "SELECT * FROM Product";
+                $result = $this->conn->query($sql);
+                $products = [];
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $products[] = $row;
+                    }
+                }
+
+                return $products;
+            }
+
+            public function displayProducts() {
+                $products = $this->getAllProducts();
+
+                if (count($products) > 0) {
+                    echo "<div class='row'>";
+
+                    foreach ($products as $product) {
+                        echo "<div class='col-md-4 mb-4'>
+                                <div class='card'>
+                                    <img src='{$product['Image']}' class='card-img-top' alt='{$product['ProductName']}'>
+                                    <div class='card-body'>
+                                        <h5 class='card-title'>{$product['ProductName']}</h5>
+                                        <p class='card-text'>{$product['Description']}</p>
+                                    
+                                        <p class='card-text'>Product Type: {$product['ProductType']}</p>
+                                        
+                                        <p class='card-text'>Price: {$product['Price']}</p>
+                                    </div>
+                                </div>
+                            </div>";
+                    }
+
+                    echo "</div>";
+                } else {
+                    echo "No products found.";
+                }
+            }
+
         }
-    }
 
-    public function __destruct() {
-        $this->conn->close();
-    }
-}
+        $host = "localhost:3307";
+        $username = "root";
+        $password = "CG-vak123";
+        $database = "mydb";
 
-$host = "localhost:3307";
-$username = "root";
-$password = "CG-vak123";
-$database = "mydb";
+        $productManager = new Product($host, $username, $password, $database);
+        $productManager->displayProducts();
+        ?>
+    </div>
 
-$productManager = new Product($host, $username, $password, $database);
-$productManager->displayProducts();
-?>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
--- src/testfor.php
+++ src/testfor.php
@@ -95,3 +95,75 @@ if ($result->num_rows > 0) {
       echo "<p>Welcome! Please sign in.</p>";
   }
 ?> -->
+
+<?php
+class Product {
+    private $conn;
+
+    public function __construct($host, $username, $password, $database) {
+        $this->conn = new mysqli($host, $username, $password, $database);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getAllProducts() {
+        $sql = "SELECT * FROM Product";
+        $result = $this->conn->query($sql);
+        $products = [];
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $products[] = $row;
+            }
+        }
+
+        return $products;
+    }
+
+    public function displayProducts() {
+        $products = $this->getAllProducts();
+
+        if (count($products) > 0) {
+            echo "<table border='1'>
+                    <tr>
+                        <th>Product Code</th>
+                        <th>Product Name</th>
+                        <th>Product Type</th>
+                        <th>Description</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                    </tr>";
+
+            foreach ($products as $product) {
+                echo "<tr>
+                        <td>{$product['ProductCode']}</td>
+                        <td>{$product['ProductName']}</td>
+                        <td>{$product['ProductType']}</td>
+                        <td>{$product['Description']}</td>
+                        <td>{$product['Quantity']}</td>
+                        <td>{$product['Price']}</td>
+                        <td><img src='{$product['Image']}' alt='{$product['ProductName']}' width='100'></td>
+                    </tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "No products found.";
+        }
+    }
+
+    public function __destruct() {
+        $this->conn->close();
+    }
+}
+
+$host = "localhost:3307";
+$username = "root";
+$password = "CG-vak123";
+$database = "mydb";
+
+$productManager = new Product($host, $username, $password, $database);
+$productManager->displayProducts();
+?>
