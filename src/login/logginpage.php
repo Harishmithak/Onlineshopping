da1f2b6bf4135d6f89478b92f3d3e99ad568c69f
harishmithak <kharishmitha@gmail.com>
2023-08-19 16:47:30 +0530

    saturday

---
+++ src/login/logginpage.php
@@ -0,0 +1,66 @@
+
+
+<?php
+session_start();
+include("../common/connect.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $usertype = $_POST['usertype'];
+
+    $query = "SELECT UserType FROM User WHERE Username = ? AND Password = ?";
+    
+    $stmt = $conn->prepare($query);
+    if (!$stmt) {
+        die("Error in preparing statement: " . $conn->error);
+    }
+    
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $stmt->bind_result($userType);
+    $stmt->fetch();
+    $stmt->close();
+
+    if ($userType === 'user' || $userType === 'admin') {
+        $_SESSION['id']=$id;
+        $_SESSION['username'] = $username;
+        $_SESSION['email'] = $email;
+        $_SESSION['login_time'] = date('Y-m-d H:i:s');
+        if (!isset($_SESSION['login_count'])) {
+            $_SESSION['login_count'] = 1;
+        } else {
+            $_SESSION['login_count']++;
+        }
+ 
+        setcookie('stored_username', $username, time() + 6, "/");
+        setcookie('stored_email', $email, time() + 6, "/");
+        
+  
+        
+        $login_time = date('Y-m-d H:i:s'); 
+        $insert_query = "INSERT INTO UserLog (username, email, login_time, usertype) VALUES (?, ?, ?, ?)";
+        
+        $insert_stmt = $conn->prepare($insert_query);
+        if (!$insert_stmt) {
+            die("Error in preparing insert statement: " . $conn->error);
+        }
+        
+        $insert_stmt->bind_param("ssss", $username, $email, $login_time, $usertype);
+        $insert_stmt->execute();
+        $insert_stmt->close();
+
+        if ($usertype === 'user') {
+            header('Location: ../product/userproduct.php');
+            exit();
+        } elseif ($usertype === 'admin') {
+            header('Location: ../product/product.php');
+            exit();
+        }
+    } else {
+     
+    }
+}
+?>
+
---
+++ src/product/userproduct.php
@@ -0,0 +1,63 @@
+<?php
+include("../common/nav.php");
+
+include("../common/connect.php");
+
+$productQuery = "SELECT * FROM Product";
+$productResult = $conn->query($productQuery);
+
+$userQuery = "SELECT * FROM User";
+$userResult = $conn->query($userQuery);
+
+?>
+
+
+<html>
+<head>
+    <title>Product and User Details</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+        
+        h2 {
+            margin-top: 20px;
+        }
+
+        .card {
+            margin: 20px;
+            width: 18rem;
+            display: inline-block;
+        }
+
+        img.card-img-top {
+            max-height: 200px;
+            object-fit: cover;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h2>Product Details</h2>
+        <div class="row">
+            <?php while ($productRow = $productResult->fetch_assoc()): ?>
+                <div class="card">
+                    <img class="card-img-top"  src="<?= $productRow['Image'] ?>" alt="Product Image">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $productRow['ProductName'] ?></h5>
+                        <p class="card-text"><?= $productRow['Description'] ?></p>
+                        <p class="card-text">Price: <?= $productRow['Price'] ?></p>
+ 
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+</body>
+
+</html>
